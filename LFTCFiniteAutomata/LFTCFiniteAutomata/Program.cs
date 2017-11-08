b45501df78318a3004be84359c49ee6f8f8197d2
Daniel Dume <daniel.dume@qubiz.com>
2017-11-08 09:54:05 +0200

    empty project (#1)

---
+++ LFTCFiniteAutomata/LFTCFiniteAutomata/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LFTCFiniteAutomata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
